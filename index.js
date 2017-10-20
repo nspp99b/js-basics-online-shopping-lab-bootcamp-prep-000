b049e1513ce627c28fd38a9313df2d5c9d9d5c88
Shahin Motia <chromepanthers@gmail.com>
2017-10-20 02:29:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0, i < cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) {
     if cart[i].hasOwnProperty(item) {
-      cart.splice(i,1)
-      return cart
+      cart.splice(i,1);
+      return cart;
     }
     else {
       console.log('That item is not in your cart.')
