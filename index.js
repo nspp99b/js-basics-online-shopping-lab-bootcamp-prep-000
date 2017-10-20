c38402759a1128bb3992e54d367d1c457de1335a
Shahin Motia <chromepanthers@gmail.com>
2017-10-20 03:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,8 +74,7 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
-    total()
-    console.log(`Your total cost is ${cartTotal}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
   }
   setCart([])
 }
