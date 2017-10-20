e42083e0b0bbe6c7a0f7d14b95c1a42958493aa3
Shahin Motia <chromepanthers@gmail.com>
2017-10-20 01:22:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,20 +24,18 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  var cartContents = []
+  var cartContents = [];
   if (cart.length === 0) {
     console.log("Your shopping cart is empty.");
   }
   else {
-
     if (cart.length === 1) {
       console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`);
-      cartContents = []
     }
     else if (cart.length === 2) {
       cartContents.push(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}`);
       cartContents.push(`and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`);
-      console.log(cartContents);
+      console.log(`${cartContents}`);
     }
     else {
       cartContents.push("In your cart, you have");
@@ -49,7 +47,7 @@ function viewCart() {
           cartContents.push(`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]},`)
         }
       }
-      console.log(...cartContents);
+      console.log(`${cartContents}`);
     }
   }
   cartContents = []
