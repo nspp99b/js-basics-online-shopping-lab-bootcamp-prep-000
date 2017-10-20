307e5684e0e7ceb94c2ae7c0c57bf23ed74b97e5
Shahin Motia <chromepanthers@gmail.com>
2017-10-20 01:01:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function viewCart() {
     else if (cart.length === 2) {
       cartContents.push(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}`);
       cartContents.push(`and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`);
-      console.log(...cartContents);
+      console.log(cartContents);
     }
     else {
       cartContents.push("In your cart, you have");
