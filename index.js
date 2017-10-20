5dd229444167c1b2bb0efaba056ea44864b69254
Shahin Motia <chromepanthers@gmail.com>
2017-10-20 03:38:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber = null) {
+  if (cardNumber === null) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
   else {
