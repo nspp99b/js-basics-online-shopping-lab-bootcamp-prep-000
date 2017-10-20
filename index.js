609dfe1ece3762f1d0c63eaf44570b6199f625f1
Shahin Motia <chromepanthers@gmail.com>
2017-10-20 02:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
-    if cart[i].hasOwnProperty(item) {
+    if (cart[i].hasOwnProperty(item) === true) {
       cart.splice(${i},1);
       return cart;
     }
